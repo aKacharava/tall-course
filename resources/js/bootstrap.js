e39053747cf158bd522f2529d7cf50dae5b5a713
Alexander Kacharava <alexander@cube.nl>
2024-08-09 11:40:22 +0200

    Initialize alpinejs

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,10 @@
 import axios from 'axios';
+import { Alpine } from "../../vendor/livewire/livewire/dist/livewire.esm.js";
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.Alpine = Alpine;
+
+Alpine.start();
