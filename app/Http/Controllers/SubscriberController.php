c781adff19c27bbc55275c4d2d689ce134c9f82b
Alexander Kacharava <alexander@cube.nl>
2024-08-12 10:10:52 +0200

    Edit email verify url

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Subscriber;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
 
 class SubscriberController extends Controller
 {
-    //
+    public function verify(Subscriber $subscriber): Application|Redirector|RedirectResponse
+    {
+        if(!$subscriber->hasVerifiedEmail()){
+            $subscriber->markEmailAsVerified();
+        }
+
+        return redirect('/?verified=1');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\SubscriberController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
+
+Route::get('/subscribers/verify/{subscriber}', [SubscriberController::class, 'verify'])
+    ->middleware('signed')
+    ->name('subscribers.verify');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
