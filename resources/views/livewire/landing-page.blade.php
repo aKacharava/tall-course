2ff8d82c4cee135a6b9de44aaeb68a05f522dbb2
Alexander Kacharava <alexander@cube.nl>
2024-08-12 09:24:43 +0200

    Edit modal template and apply component

--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -1,7 +1,7 @@
 <div
     x-data="{
         showSubscribe: false,
-        showSuccess: true
+        showSuccess: false
     }"
     class="flex flex-col bg-indigo-900 w-full h-screen"
 >
@@ -37,55 +37,47 @@
             </x-secondary-button>
         </div>
     </div>
-    <div
-        x-show="showSubscribe"
-        x-on:click.self="showSubscribe = false"
-        x-on:keydown.escape.window="showSubscribe = false"
-        class="flex fixed top-0 bg-gray-900 bg-opacity-60 items-center w-full h-full"
+    <x-modal
+        trigger="showSubscribe"
+        class="bg-pink-500"
     >
-        <div class="m-auto bg-pink-700 shadow-2xl rounded-xl p-8">
-            <p class="text-white text-5xl font-extrabold text-center">
-                Let's do it!
-            </p>
-            <form
-                wire:submit="subscribe"
-                class="flex flex-col items-center p-24"
+        <p class="text-white text-5xl font-extrabold text-center">
+            Let's do it!
+        </p>
+        <form
+            wire:submit="subscribe"
+            class="flex flex-col items-center p-24"
+        >
+            <x-text-input
+                wire:model="email"
+                class="px-5 py-3 w-80 border border-blue-400 !bg-white !text-gray-800 placeholder:!text-gray-400"
+                type="email"
+                name="email"
+                placeholder="Email address"
+            />
+            <span class="text-gray-100 text-xs">
+                    We will send you a confirmation email.
+                </span>
+            <x-secondary-button
+                wire:click="subscribe"
+                class="px-5 py-3 mt-5 w-80 !bg-blue-500 hover:!bg-blue-700 justify-center"
             >
-                <x-text-input
-                    wire:model="email"
-                    class="px-5 py-3 w-80 border border-blue-400 !bg-white !text-gray-800 placeholder:!text-gray-400"
-                    type="email"
-                    name="email"
-                    placeholder="Email address"
-                />
-                <span class="text-gray-100 text-xs">
-                        We will send you a confirmation email.
-                    </span>
-                <x-secondary-button
-                    wire:click="subscribe"
-                    class="px-5 py-3 mt-5 w-80 !bg-blue-500 hover:!bg-blue-700 justify-center"
-                >
-                    Get in
-                </x-secondary-button>
-            </form>
-        </div>
-    </div>
-    <div
-        x-show="showSuccess"
-        x-on:click.self="showSuccess = false"
-        x-on:keydown.escape.window="showSuccess = false"
-        class="flex fixed top-0 bg-gray-900 bg-opacity-60 items-center w-full h-full"
+                Get in
+            </x-secondary-button>
+        </form>
+    </x-modal>
+    <x-modal
+        trigger="showSuccess"
+        class="bg-green-500"
     >
-        <div class="m-auto bg-green-700 shadow-2xl rounded-xl p-8">
-            <p class="animate-pulse text-white text-9xl font-extrabold text-center">
-                &check;
-            </p>
-            <p class="text-white text-5xl font-extrabold text-center mt-16">
-                Great!
-            </p>
-            <p class="text-white text-3xl text-center">
-                Check your inbox
-            </p>
-        </div>
-    </div>
+        <p class="animate-pulse text-white text-9xl font-extrabold text-center">
+            &check;
+        </p>
+        <p class="text-white text-5xl font-extrabold text-center mt-16">
+            Great!
+        </p>
+        <p class="text-white text-3xl text-center">
+            Check your inbox
+        </p>
+    </x-modal>
 </div>
