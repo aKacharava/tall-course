50d451f3abb72a90142334e1b5873eebf7783461
Alexander Kacharava <alexander@cube.nl>
2024-08-12 14:05:43 +0200

    Add search functionality with livewire

--- resources/views/livewire/subscribers.blade.php
+++ resources/views/livewire/subscribers.blade.php
@@ -3,44 +3,52 @@
         Subscribers
     </h1>
 
-    @if($subscribers->isEmpty())
-        <div class="flex w-full bg-red-800 p-5 rounded-lg">
-            <p class="text-red-500">
-                No subscribers
-            </p>
-        </div>
-    @else
-        <table class="w-full">
-            <thead class="border-b-2 border-gray-600 text-indigo-400">
-            <tr>
-                <th class="px-6 py-3 text-left">Email</th>
-                <th class="px-6 py-3 text-left">Verified</th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($subscribers as $subscriber)
-                <tr class="text-sm text-indigo-300 border-b border-gray-400">
-                    <td class="px-6 py-4">
-                        {{ $subscriber->email }}
-                    </td>
-                    <td class="px-6 py-4">
-                        {{
-                            optional($subscriber->email_verified_at)
-                                ->diffForHumans() ?? 'Never'
-                        }}
-                    </td>
-                    <td class="px-6 py-4">
-                        <x-secondary-button
-                            class="!text-red-100 !bg-red-700 hover:!bg-red-900"
-                            wire:click="delete({{ $subscriber->id }})"
-                        >
-                            A button
-                        </x-secondary-button>
-                    </td>
+    <div class="px-8">
+        <x-text-input
+            type="text"
+            class="rounded-lg border float-right border-gray-300 mb-4 pl-8 w-1/3"
+            placeholder="Search"
+            wire:model.live="search"
+        />
+        @if($subscribers->isEmpty())
+            <div class="flex w-full bg-red-800 p-5 rounded-lg">
+                <p class="text-red-500">
+                    No subscribers found.
+                </p>
+            </div>
+        @else
+            <table class="w-full">
+                <thead class="border-b-2 border-gray-600 text-indigo-400">
+                <tr>
+                    <th class="px-6 py-3 text-left">Email</th>
+                    <th class="px-6 py-3 text-left">Verified</th>
+                    <th></th>
                 </tr>
-            @endforeach
-            </tbody>
-        </table>
-    @endif
+                </thead>
+                <tbody>
+                @foreach($subscribers as $subscriber)
+                    <tr class="text-sm text-indigo-300 border-b border-gray-400">
+                        <td class="px-6 py-4">
+                            {{ $subscriber->email }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{
+                                optional($subscriber->email_verified_at)
+                                    ->diffForHumans() ?? 'Never'
+                            }}
+                        </td>
+                        <td class="px-6 py-4">
+                            <x-secondary-button
+                                class="!text-red-100 !bg-red-700 hover:!bg-red-900"
+                                wire:click="delete({{ $subscriber->id }})"
+                            >
+                                A button
+                            </x-secondary-button>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        @endif
+    </div>
 </div>
